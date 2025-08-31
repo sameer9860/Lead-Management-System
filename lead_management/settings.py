c6c794cbfacd01808c3ae69b4bc7cfa1e725d306
sameer9860 <samirkhatiwada68@gmail.com>
2025-08-31 21:08:07 +0545

    fix : Update Lead

--- lead_management/settings.py
+++ lead_management/settings.py
@@ -13,19 +13,20 @@ import os
 
 
 from pathlib import Path
-from dotenv import Config
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Config('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
