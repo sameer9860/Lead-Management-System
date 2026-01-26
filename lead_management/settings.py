f8ec15218d1f8185ac49e461d68506529f82241b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-01-26 18:30:38 +0545

    Allowedhost

--- lead_management/settings.py
+++ lead_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['Samiir.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
